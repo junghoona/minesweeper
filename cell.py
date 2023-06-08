7ae1c840685145d7940c87aa14d28f89c79abde9
John An <junghooonan@gmail.com>
2023-06-08 14:50:23 -0700

    Finishing touches and playing the game

--- cell.py
+++ cell.py
@@ -1,16 +1,18 @@
 from tkinter import Button, Label
 import random 
 import settings
+import sys
+import os
 
 
 class Cell: 
     all = []
     cell_count = settings.CELL_COUNT
     cell_count_label_obj = None 
-    
     def __init__(self, x, y, is_mine=False): 
         self.is_mine = is_mine 
         self.is_opened = False 
+        self.is_mine_candidate = False 
         self.cell_btn_obj= None 
         self.x = x
         self.y = y
@@ -25,7 +27,7 @@ class Cell:
             height=4
         )
         btn.bind('<Button-1>', self.left_click_actions)     # Left Click 
-        btn.bind('<Button-3>', self.right_click_actions)    # Right Click
+        btn.bind('<Button-2>', self.right_click_actions)    # Right Click
         self.cell_btn_obj = btn 
     
     # Decorator for use case of the class, not every instance 
@@ -48,6 +50,13 @@ class Cell:
                 for cell_btn_obj in self.surrounded_cells: 
                     cell_btn_obj.show_cell() 
             self.show_cell()
+            # If mines_count is equal to the cells left count, player won 
+            if Cell.cell_count == settings.MINES_COUNT: 
+                os.system("osascript -e 'Tell application \"System Events\" to display dialog \"Congratulations! You won the game\" with title \"Game Over\"'") 
+                
+        # Cancel Left and Right click events if cell is already opened 
+        self.cell_btn_obj.unbind('<Button-1>') 
+        self.cell_btn_obj.unbind('<Button-2>')
     
     def get_cell_by_axis(self, x, y): 
         # Return a cell object based on values of x and y
@@ -88,17 +97,31 @@ class Cell:
                 Cell.cell_count_label_obj.config(
                     text=f"Cells Left: {Cell.cell_count}",
                 )
+            # If this was a mine candidate, then for safety,
+            # configure the background color to SystemButtonFace 
+            self.cell_btn_obj.config(
+                bg='SystemButtonFace',
+            )
+            
         # Mark the cell as opened (Use this as the last line of this method) 
         self.is_opened = True 
     
+    # A logic to interrupt the game and display a message that player lost
     def show_mine(self):
-        # A logic to interrupt the game and display a message that player lost
-        self.cell_btn_obj.config(bg='red') 
+        self.cell_btn_obj.config(bg='red')
+        os.system("osascript -e 'Tell application \"System Events\" to display dialog \"You clicked on a mine\" with title \"Game Over\"'")  
+        sys.exit()
     
     def right_click_actions(self, event): 
-        print(event) 
-        print("I am right clicked!")
-    
+        if not self.is_mine_candidate: 
+            self.cell_btn_obj.config(
+                bg='orange' 
+            )
+            self.is_mine_candidate = True 
+        else: 
+            self.cell_btn_obj.config(
+                bg='SystemButtonFace' 
+            )
     
     @staticmethod
     def randomize_mines(): 
